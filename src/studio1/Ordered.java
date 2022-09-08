c5354e2faa0ec4bcd68d1414457fd9fe23e5100b
h8470 <h8470@DESKTOP-HFGSJU9.dhcp.wustl.edu>
2022-09-08 14:59:25 -0400

    Finished

--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -15,7 +15,10 @@ public class Ordered {
 		int y = in.nextInt();
 		System.out.println("Value for z?");
 		int z = in.nextInt();
-
+		boolean xy = x > y && y >z; 
+		boolean yz = z > y && y > x;
+		boolean isOrdered = xy || yz;
+		System.out.println(isOrdered);
 	}
 
 }
